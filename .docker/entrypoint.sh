2b026448d5280d43865bb0697824f8a101970709
Julia <julia.marsal.robotics@gmail.com>
2024-01-13 13:52:18 +0000

    adding left packages to make the overall simulation work

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -3,7 +3,7 @@ set -e
 # setup ros environment
 source "/opt/ros/noetic/setup.bash"
 source "/dev_ws/devel/setup.bash"
-
+export GAZEBO_MODEL_PATH=/usr/share/gazebo-11/models:/dev_ws/src/gazebo_models
 # export ROS_MASTER_URI=http://10.41.1.1:11311
 # export ROS_IP=10.41.1.10
 
